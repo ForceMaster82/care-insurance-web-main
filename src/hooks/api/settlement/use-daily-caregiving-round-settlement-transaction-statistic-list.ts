e6a264e946c21f2029b095110327a90d805660c8
force8211 <force8211@cpst.co.kr>
2024-03-10 14:01:43 +0900

    간병현황 - 고객명 param 추가

--- src/hooks/api/settlement/use-daily-caregiving-round-settlement-transaction-statistic-list.ts
+++ src/hooks/api/settlement/use-daily-caregiving-round-settlement-transaction-statistic-list.ts
@@ -1,35 +1,53 @@
 import {useQuery} from '@tanstack/react-query'
 import DailyCaregivingRoundSettlementTransactionStatisticResource from '~models/dto/daily-caregiving-round-settlement-transaction-statistic/Resource'
-import {PaginationParams} from '~types'
+import {PaginationParams, SearchCategory} from '~types'
 import {
     IDailyCaregivingRoundTransactionStatistic,
     IPaginationResponse,
 } from '~types/dto'
 import {fetcher} from '~utils/fetcher'
+import {formatSearchQuery, getURLSearchParams} from "utils/url";
 
 interface IProps extends PaginationParams {
     date: string,
-    searchCategory: string,
+    searchCategory: SearchCategory,
     searchKeyword: string,
 }
 
-const useDailyCaregivingRoundSettlementTransactionStatisticList = ({
-   date,
+const useDailyCaregivingRoundSettlementTransactionStatisticList = (
+   /*date,
    pageNumber,
    pageSize,
-    searchKeyword,
-    searchCategory,
-}: IProps):
-    | IPaginationResponse<DailyCaregivingRoundSettlementTransactionStatisticResource>
-    | undefined => {
+   searchKeyword,
+   searchCategory,*/
+   props: IProps
+): IPaginationResponse<DailyCaregivingRoundSettlementTransactionStatisticResource> | undefined => {
+    const {
+        date,
+        pageNumber,
+        pageSize,
+        searchCategory,
+        searchKeyword,
+    } = props
 
-    const qt = searchKeyword ? `query=${searchCategory}:${searchKeyword}` : '';
+    const searchQuery = formatSearchQuery(searchCategory, searchKeyword)
+    //const qt = searchKeyword ? `query=${searchCategory}:${searchKeyword}` : '';
+
+    const queryParams = getURLSearchParams([
+        ...Object.entries({
+            date,
+            pageNumber,
+            pageSize,
+            query: searchQuery,
+        }),
+    ])
 
     const {data} = useQuery({
         keepPreviousData: true,
         queryFn: () =>
             fetcher<IPaginationResponse<IDailyCaregivingRoundTransactionStatistic>>(
-                `/api/v1/daily-caregiving-round-settlement-transaction-statistics?date=${date}&page-number=${pageNumber}&page-size=${pageSize}&${qt}`,
+                //`/api/v1/daily-caregiving-round-settlement-transaction-statistics?date=${date}&page-number=${pageNumber}&page-size=${pageSize}&${searchQuery}`,
+                `/api/v1/daily-caregiving-round-settlement-transaction-statistics?${queryParams}`,
             ),
         queryKey: [
             'daily-caregiving-round-settlement-transaction-statistic',
--- src/hooks/api/settlement/use-daily-settlement-transaction-statistic.ts
+++ src/hooks/api/settlement/use-daily-settlement-transaction-statistic.ts
@@ -2,27 +2,40 @@ import {useQuery} from '@tanstack/react-query'
 import DailySettlementTransactionStatisticResource from '~models/dto/daily-settlement-transaction-statistic/Resource'
 import {IDailyTransactionStatistic} from '~types/dto'
 import {fetcher} from '~utils/fetcher'
+import {SearchCategory} from "types";
+import {formatSearchQuery, getURLSearchParams} from "utils/url";
 
 interface IProps {
   date: string,
-  searchCategory: string,
+  searchCategory: SearchCategory,
   searchKeyword: string,
 }
 
-const useDailySettlementTransactionStatistic = ({
-  date,
-  searchKeyword,
-  searchCategory,
-}: IProps): DailySettlementTransactionStatisticResource | undefined => {
-  const qt = searchKeyword ? `query=${searchCategory}:${searchKeyword}` : '';
+const useDailySettlementTransactionStatistic = (
+  props: IProps
+): DailySettlementTransactionStatisticResource | undefined => {
+  const {
+    date,
+    searchCategory,
+    searchKeyword,
+  } = props
+
+  const searchQuery = formatSearchQuery(searchCategory, searchKeyword)
+
+  const queryParams = getURLSearchParams([
+    ...Object.entries({
+      date,
+      query: searchQuery,
+    }),
+  ])
 
   const {data} = useQuery({
     keepPreviousData: true,
     queryFn: () =>
       fetcher<IDailyTransactionStatistic[]>(
-        `/api/v1/daily-settlement-transaction-statistics?date=${date}&${qt}`,
+        `/api/v1/daily-settlement-transaction-statistics?${queryParams}`,
       ),
-    queryKey: ['daily-settlement-transaction-statistic', {date}],
+    queryKey: ['daily-settlement-transaction-statistic', {date},],
     select: (response) =>
       response.body.length > 0
         ? new DailySettlementTransactionStatisticResource(response.body[0])
