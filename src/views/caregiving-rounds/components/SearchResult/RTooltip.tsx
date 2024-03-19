c8ca947b33f197f2394c3a44ad4f08771c60cdfb
force8211 <force8211@cpst.co.kr>
2024-03-19 10:32:14 +0900

    고객관리, 접수관리 > 연락처검색_보호자,간병인 / 병원명검색 > 요약정보 툴팁으로 추가

---
+++ src/views/caregiving-rounds/components/SearchResult/RTooltip.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+type Props = {
+    children: React.ReactNode | React.ReactNode[];
+    message: string;
+    className?: string;
+}
+
+const RTooltip: React.FC<Props> = ({ children, message, className }) => {
+    // @ts-ignore
+    let scRTooltip = <><ScRTooltip className={className}>
+        {children}
+        <p className='tooltip'>{message}</p>
+    </ScRTooltip></>;
+    return scRTooltip
+}
+
+const ScRTooltip = styled.span`
+  position: relative;
+  
+  .tooltip {
+    position: absolute;
+    left: -100%;
+    top: -100%;
+    min-width: 250px;
+    min-height: 60px;
+    padding: 10px;
+    background-color: #777;
+    color: #fff;
+    font-size: 15px;
+    border-radius: 10px;
+    z-index: 10;
+    display: none;
+  }
+
+  &:hover {
+    .tooltip {
+      display: block;
+    }
+  }
+`
+
+export default RTooltip
